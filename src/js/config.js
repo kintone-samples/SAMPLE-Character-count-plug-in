f7b3dfe9fd51cab3966731d120c01dff8eb01948
MaedaAs <maeda_asami@comture.com>
2019-08-08 16:23:03 +0900

    Edited indent spacing from 4 to 2 spaces

--- src/js/config.js
+++ src/js/config.js
@@ -6,72 +6,72 @@ https://github.com/kintone/SAMPLE-Character-count-plug-in/blob/master/LICENSE
 
 jQuery.noConflict();
 (function($, PLUGIN_ID) {
-    'use strict';
-    // Get configuration settings
+  'use strict';
+  // Get configuration settings
 
-    var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
+  var CONF = kintone.plugin.app.getConfig(PLUGIN_ID);
 
-    function escapeHtml(htmlstr) {
-        return htmlstr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;').replace(/'/g, '&#39;');
-    }
+  function escapeHtml(htmlstr) {
+    return htmlstr.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;').replace(/'/g, '&#39;');
+  }
 
-    function setDropDown() {
-        // Retrieve field information, then set drop-down
-        return kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET',
-            {'app': kintone.app.getId()}).then(function(resp) {
+  function setDropDown() {
+    // Retrieve field information, then set drop-down
+    return kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET',
+      {'app': kintone.app.getId()}).then(function(resp) {
 
-            for (var key in resp.properties) {
-                if (!resp.properties.hasOwnProperty(key)) {
-                    continue;
-                }
-                var prop = resp.properties[key];
-                var $option = $('<option>');
+      for (var key in resp.properties) {
+        if (!resp.properties.hasOwnProperty(key)) {
+          continue;
+        }
+        var prop = resp.properties[key];
+        var $option = $('<option>');
 
-                switch (prop.type) {
-                    case 'MULTI_LINE_TEXT':
-                        $option.attr('value', prop.code);
-                        $option.text(escapeHtml(prop.label));
-                        $('#select_body_field').append($option.clone());
-                        break;
-                    case 'NUMBER':
-                        $option.attr('value', prop.code);
-                        $option.text(escapeHtml(prop.label));
-                        $('#select_count_field').append($option.clone());
-                        break;
-                    default:
-                        break;
-                }
-            }
-            // Set default values
-            $('#select_body_field').val(CONF.body);
-            $('#select_count_field').val(CONF.count);
-        }, function(resp) {
-            return alert('Failed to retrieve field information');
-        });
-    }
-    $(document).ready(function() {
-        // Set drop-down list
-        setDropDown();
-        // Set input values when 'Save' button is clicked
-        $('#check-plugin-submit').click(function() {
-            var config = [];
-            var body = $('#select_body_field').val();
-            var count = $('#select_count_field').val();
-
-            // Check required fields
-            if (body === '' || count === '') {
-                alert('Please set required field(s)');
-                return;
-            }
-            config.body = body;
-            config.count = count;
-
-            kintone.plugin.app.setConfig(config);
-        });
-        // Process when 'Cancel' is clicked
-        $('#check-plugin-cancel').click(function() {
-            history.back();
-        });
+        switch (prop.type) {
+          case 'MULTI_LINE_TEXT':
+            $option.attr('value', prop.code);
+            $option.text(escapeHtml(prop.label));
+            $('#select_body_field').append($option.clone());
+            break;
+          case 'NUMBER':
+            $option.attr('value', prop.code);
+            $option.text(escapeHtml(prop.label));
+            $('#select_count_field').append($option.clone());
+            break;
+          default:
+            break;
+        }
+      }
+      // Set default values
+      $('#select_body_field').val(CONF.body);
+      $('#select_count_field').val(CONF.count);
+    }, function(resp) {
+      return alert('Failed to retrieve field information');
     });
+  }
+  $(document).ready(function() {
+    // Set drop-down list
+    setDropDown();
+    // Set input values when 'Save' button is clicked
+    $('#check-plugin-submit').click(function() {
+      var config = [];
+      var body = $('#select_body_field').val();
+      var count = $('#select_count_field').val();
+
+      // Check required fields
+      if (body === '' || count === '') {
+        alert('Please set required field(s)');
+        return;
+      }
+      config.body = body;
+      config.count = count;
+
+      kintone.plugin.app.setConfig(config);
+    });
+    // Process when 'Cancel' is clicked
+    $('#check-plugin-cancel').click(function() {
+      history.back();
+    });
+  });
 })(jQuery, kintone.$PLUGIN_ID);
--- src/js/desktop.js
+++ src/js/desktop.js
@@ -7,36 +7,36 @@
  * Licensed under the MIT License
  */
 (function(PLUGIN_ID) {
-    'use strict';
+  'use strict';
 
-    // Get plug-in configuration settings
-    var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
-    // Get each settings
-    if (!CONFIG) {
-        return false;
-    }
+  // Get plug-in configuration settings
+  var CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID);
+  // Get each settings
+  if (!CONFIG) {
+    return false;
+  }
 
-    var CONFIG_BODY = CONFIG.body;
-    var CONFIG_COUNT = CONFIG.count;
+  var CONFIG_BODY = CONFIG.body;
+  var CONFIG_COUNT = CONFIG.count;
 
-    kintone.events.on(['app.record.create.submit', 'app.record.edit.submit', 'app.record.index.edit.submit'],
-        function(event) {
-            // Obtain characters in the text field
-            var rec = event.record;
-            var st = rec[CONFIG_BODY].value;
+  kintone.events.on(['app.record.create.submit', 'app.record.edit.submit', 'app.record.index.edit.submit'],
+    function(event) {
+      // Obtain characters in the text field
+      var rec = event.record;
+      var st = rec[CONFIG_BODY].value;
 
-            // If the number of characters is zero, put zero in number field and return
-            if (!st) {
-                rec[CONFIG_COUNT].value = 0;
-                return event;
-            }
+      // If the number of characters is zero, put zero in number field and return
+      if (!st) {
+        rec[CONFIG_COUNT].value = 0;
+        return event;
+      }
 
-            //Remove spaces
-            var st2 = st.replace(/\s+/g, "");
+      //Remove spaces
+      var st2 = st.replace(/\s+/g, "");
 
-            //Enter character count into number field
-            rec[CONFIG_COUNT].value = st2.length;
+      //Enter character count into number field
+      rec[CONFIG_COUNT].value = st2.length;
 
-            return event;
-        });
+      return event;
+    });
 })(kintone.$PLUGIN_ID);
